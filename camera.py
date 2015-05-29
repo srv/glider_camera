fd746a1a411f493677899fd76c5b5ded542a31a4
SOCIB-RPi2 <socib@raspberry>
2015-05-29 15:14:14 +0000

    automatic shutdown

--- camera.py
+++ camera.py
@@ -28,8 +28,9 @@ class GliderCamera:
     print "\t* End date:  %s" % self.end_date
     print "\t* Frequency: %s" % self.freq
 
-  def shutdown():
-    command = "/usr/bin/sudo /sbin/shutdown -p now"
+  def shutdown(self):
+    #TODO: change shutdown to power off
+    command = "/usr/bin/sudo /sbin/shutdown -k now"
     import subprocess
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
@@ -37,27 +38,22 @@ class GliderCamera:
 
   def capture(self):
     self.camera = picamera.PiCamera()
-    self.camera.start_preview()
-    time.sleep(2)
-    t = datetime.now()
-    print t
-    print self.init_date
+    '''self.camera.start_preview()'''
+    time.sleep(1)
     path = os.getcwd() + "/" + self.mission_name
     if not os.path.exists(path):
       os.makedirs(path)
-    if t < self.init_date:
+    if datetime.now() < self.init_date:
       d = self.init_date - t
       print "Capture will start in " + repr(d.days) + " days..."
-    while t < self.init_date:
+    while datetime.now() < self.init_date:
       time.sleep(10)
-      t = datetime.now()
-    if t > self.init_date:
-      print "Starting capture..."
+    while datetime.now() > self.init_date and datetime.now() < self.end_date:
       image_time = time.strftime("%Y%m%d-%H%M%S")
-      for filename in self.camera.capture_continuous(path + '/img-' + image_time + '-{counter:04d}.jpg'):
-        print('Captured %s' % filename)
-        time.sleep(self.freq)
-    elif t < self.end_date:
+      self.camera.capture(path + '/img-' + image_time + '.jpg')
+      print("Saved img-" + image_time + ".jpg")
+      time.sleep(self.freq)
+    if datetime.now() > self.end_date:
       print "Capture time ended. Shutdown..."
       self.shutdown()
 
