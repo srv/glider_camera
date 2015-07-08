25853c79acce340c17117c438ba61a874e54d141
raspberry <socib@raspberry>
2015-07-08 13:19:26 +0000

    added doc comments

--- camera.py
+++ camera.py
@@ -8,13 +8,13 @@ import RPi.GPIO as GPIO
 
 class GliderCamera:
   def __init__(self):
+    """ Constructor of GliderCamera class """
     print "[GliderCamera]"
     self.mission_name = "test"
     self.start_date = datetime.now()
     self.end_date = self.start_date
     self.period = 10
-    self.readConfig("config.yaml")
-    self.readCameraConfig("camera_config.yaml")
+    self.readConfig("config.yaml", "camera_config.yaml")
     self.startLed()
     if self.mode == "master":
       self.captureMaster()
@@ -22,15 +22,18 @@ class GliderCamera:
       self.captureSlave()
 
   def __del__(self):
+    """ Destructor of GliderCamera class """
     GPIO.output(self.led_output, 0)
 
   def readCameraConfig(self, filename):
+    """ Reads the specified yaml configuration file """
     stream = open(filename, 'r')
     ys = yaml.load(stream)
     self.camera_config = ys
 
-  def readConfig(self, filename):
-    stream = open(filename, 'r')
+  def readConfig(self, config_filename, camera_config_filename):
+    """ Reads both configurations and loads their parameters to the class """
+    stream = open(config_filename, 'r')
     ys = yaml.load(stream)
     self.start_date = datetime.strptime(ys["start_date"], '%d/%m/%Y %H:%M')
     self.end_date = datetime.strptime(ys["end_date"], '%d/%m/%Y %H:%M')
@@ -38,6 +41,7 @@ class GliderCamera:
     self.mode = ys["mode"]
     self.photos_per_cycle = ys["photos_per_cycle"]
     self.period = ys["period"];
+    self.readCameraConfig(camera_config_filename)
     print "Mission configuration:"
     print "\t* Mission:          %s" % self.mission_name
     print "\t* Mode:             %s" % self.mode
@@ -48,6 +52,7 @@ class GliderCamera:
     print "\t* Period:           %s" % self.period
 
   def startLed(self):
+    """ Sets the configuration for the LED output """
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     self.led_output = self.camera_config["led_pin_output"]
@@ -58,11 +63,13 @@ class GliderCamera:
     self.led_time_off = self.camera_config["led_delay_off"]
 
   def shutdown(self):
+    """ Shutdowns the entire Raspberry """
     #TODO: change shutdown to power off
     import subprocess
     subprocess.call(["sudo",  "shutdown",  "-k",  "+10",  '"RPi2 is going down to save battery. If you are planning to work, cancel it using < sudo shutdown -c > and remember to change the end date"'])
 
   def startCamera(self):
+    """ Initializes the camera and its params """
     self.camera = picamera.PiCamera()
     width = self.camera_config["width"]
     height = self.camera_config["height"]
@@ -74,6 +81,7 @@ class GliderCamera:
     self.camera.drc_strength = self.camera_config["drc"]
 
   def captureMaster(self):
+    """ Automatic capture mode """
     self.startCamera()
     time.sleep(1)
     path = os.getcwd() + "/" + self.mission_name
@@ -91,7 +99,7 @@ class GliderCamera:
       self.shutdown()
 
   def captureSlave(self):
-    # We suppose photo cycles won't overlap
+    """ Slave capture mode. Capture cycles do NOT overlap """
     last = False
     while datetime.now() < self.end_date:
       signal_received = GPIO.input(self.signal_input)
@@ -120,6 +128,7 @@ class GliderCamera:
 
 
   def capture(self, path):
+    """ Captures an image whilst lightning the LED """
     GPIO.output(self.led_output, 1)
     time.sleep(self.led_time_on)
     ini_aux = datetime.now()
